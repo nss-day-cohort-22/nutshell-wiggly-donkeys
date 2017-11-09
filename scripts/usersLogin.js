7fc4fc577a4451694798f5941a6e03d8af3611b7
Paul Ellis <tynesellis@gmail.com>
2017-11-09 14:03:41 -0600

    initial dom population

--- scripts/usersLogin.js
+++ scripts/usersLogin.js
@@ -1,5 +1,5 @@
 const Database = JSON.parse(localStorage.getItem("Database"))//access the main database
-
+const dashboardDOM = require("./dashboard_DOM")
 const userLogin = () => {
     document.getElementById("regForm_login").addEventListener("click", login => {
         const regUsername = document.getElementById("regForm_username").value;//get value of username field
@@ -11,7 +11,7 @@ const userLogin = () => {
                 sessionStorage.setItem("currentUser", JSON.stringify(user.userId))//set session storage to new user
                 document.getElementById("regForm").classList.remove("d-flex")
                 document.getElementById("regForm").classList.add("hidden")
-                //insert populate function
+                dashboardDOM();
             }
         })
         if (match === false) {alert("I'm sorry, your info doesn't quit match.  Please re-enter or register a new account.")}
