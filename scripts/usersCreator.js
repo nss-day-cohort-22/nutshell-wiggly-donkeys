8841c8b55d8873dd2799c0344f6f0c2c180a926d
Paul Ellis <tynesellis@gmail.com>
2017-11-09 11:41:34 -0600

    login added. hide added. storage fixed

--- scripts/usersCreator.js
+++ scripts/usersCreator.js
@@ -1,10 +1,15 @@
 //Author: Paul Ellis; Purpose: create user database
 const makeUsers = require("./usersFactory");
+const Database = JSON.parse(localStorage.getItem("Database"))
+let usersDB = users => {
+    if (Database === null) {
+        const usersInit = [];
+        usersInit.push(makeUsers("Paul", "paul@nuts.com"));
+        usersInit.push(makeUsers("John", "john@nuts.com"));
+        usersInit.push(makeUsers("Max", "max@nuts.com"));
+        usersInit.push(makeUsers("Chase", "chase@nuts.com"));
+        return usersInit
+        } else {return Database.users}
+}
 
-const usersDB = [];
-usersDB.push(makeUsers("Paul", "paul@nuts.com"));
-usersDB.push(makeUsers("John", "john@nuts.com"));
-usersDB.push(makeUsers("Max", "max@nuts.com"));
-usersDB.push(makeUsers("Chase", "chase@nuts.com"));
-
-module.exports = usersDB;
+module.exports = usersDB();
