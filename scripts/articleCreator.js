4c2f0b3c6f649d2355c1c0c2f0ae72e2b8fd12a7
John Dulaney <john.s.dulaney1@gmail.com>
2017-11-09 14:04:43 -0600

    thursday afternoon git antics

--- scripts/articleCreator.js
+++ scripts/articleCreator.js
@@ -25,5 +25,19 @@ const saveArticleEl = document.getElementById("articleForm__saveButt").
     // Sort the articles by their `id` property, descending
     Database.articles.sort((p, n) => n.id - p.id);
 
+    document.forms["article__form"].reset();
+
 })
 
+
+{/* <article>
+<form name="article__form">
+    <label>Date</label>
+        <input type="date" name='articleForm__date' id="" value="Date">
+    <label>URL</label>
+        <input type="text" name='articleForm__url' value="URL">
+    <label>Note</label>
+        <textarea name="'articleForm__note'" id="note" name="Notes" cols="30" rows="10"></textarea>
+    <label>Post</label>
+        <input type="button" id="articleForm__saveButt" class="article__button" value="Post Article">
+</form> */}
--- scripts/articleDOM.js
+++ scripts/articleDOM.js
@@ -8,16 +8,6 @@ const Database = require("./Database")
 // get article
 const articleEl = document.getElementById("XXXXXXXXXXx")
 
-articleEl.innerHTML += `
-<article>
-
-<section class="article__form">
-<input type="datetime-local" name='articleForm__date' id="">
-<input type="text" name='articleForm__url'>
-<textarea name="'articleForm__note'" id="" cols="30" rows="10"></textarea>
-</section>
-
-`
 //function for scope
 const articleDOM = (article) => {
     article.forEach(artPush => {
