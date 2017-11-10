48d90ba2ca971b7138ae59811cfd8d963595e00c
John Dulaney <john.s.dulaney1@gmail.com>
2017-11-10 11:23:01 -0600

    asdf

--- scripts/articles/articleCreator.js
+++ scripts/articles/articleCreator.js
@@ -29,19 +29,11 @@ const saveArticleEl = document.getElementById("articleForm__saveButt").
     localStorage.setItem("Database", JSON.stringify(Database))        
     document.forms["article__form"].reset();
 
+})
+
+//delete button function
+function deleteCard() {
+    if (event.target.id.startsWith("button_")) {
+        event.target.parentNode.remove()
+    }
 }
-
-
-
-
-{/* <article>
-<form name="article__form">
-    <label>Date</label>
-        <input type="date" name='articleForm__date' id="" value="Date">
-    <label>URL</label>
-        <input type="text" name='articleForm__url' value="URL">
-    <label>Note</label>
-        <textarea name="'articleForm__note'" id="note" name="Notes" cols="30" rows="10"></textarea>
-    <label>Post</label>
-        <input type="button" id="articleForm__saveButt" class="article__button" value="Post Article">
-</form> */}
