9a27ad558849575eb94f227cd6ece60d449e543a
John Dulaney <john.s.dulaney1@gmail.com>
2017-11-13 11:41:20 -0600

    added events and changes to artifact/creat

--- scripts/main.js
+++ scripts/main.js
@@ -6,9 +6,6 @@ const usersLogin = require("./users/usersLogin");
 
 // creators
 const articleCreator = require("./articles/articleCreator");
-const messagesCreator = require("./messages/messagesCreator");
-const tasksCreator = require("./tasks/tasksCreator");
-const connectionsCreator = require("./connections/connectionsCreator");
 
 
 
