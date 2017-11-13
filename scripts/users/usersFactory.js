4f538f83b361c943d89635b0372b1860d1468e72
Paul Ellis <tynesellis@gmail.com>
2017-11-13 17:57:13 -0600

    database.js now an object with methods

--- scripts/users/usersFactory.js
+++ scripts/users/usersFactory.js
@@ -1,12 +1,19 @@
 //Author: Paul Ellis; Purpose: function that creates new users
-const idGenerator = require("../idGenerator");
-const userIdGen = idGenerator();
-const db = require("../Database")
-const Database = db.load();
-Database.users = Database.users || [];
+const idGenerator = require("../idGenerator");//get the idgenerator function
+const db = require("../Database");//get the database object with methods
+const Database = db.load();//use load method of database object to get local storage
+let userIdGen = null;//set instance of idGenerator
+Database.users = Database.users || [];//if Database.users doesn't exist, set to empty array
+
+if (Database.users.length === 0) {userIdGen = idGenerator()}//if there are now users yet, use idGenerator normally
+else {//if users array exists in storage....
+    Database.users.sort((p,n) => n.userId - p.userId);//..sort users array to have highest id number at index 0
+    console.log(Database.users[0])
+    userIdGen = idGenerator(Database.users[0].userId);//set instance of id generator to be passed highest id number for it's starting number
+}
 
 const usersFactory = (name, email) => {
-    let newUser =  Object.create(null, {
+    let newUser =  Object.create(null, {//create new user object
         "userId":
         {
             enumerable: true,
@@ -23,10 +30,9 @@ const usersFactory = (name, email) => {
             value: email
         }
     })
-    Database.users.push(newUser);
-    console.log(Database)
-    db.save(Database)
-    window.sessionStorage.setItem("currentUser", JSON.stringify(newUser.userId))
+    Database.users.push(newUser);//push new user into users array in Database
+    db.save(Database)//call save method of database object that pushes new db to local storage
+    sessionStorage.setItem("currentUser", JSON.stringify(newUser.userId))//set session storage to new user's id
 
 }
 
--- scripts/users/usersRegister.js
+++ scripts/users/usersRegister.js
@@ -1,23 +1,26 @@
 //Author: Paul Ellis; Purpose: event listener for registration form: make a new user object if one doesn't already exist.
-
+const db = require("../Database")
 const usersFactory = require("./usersFactory");//pull function that creates users
 // const Database = JSON.parse(localStorage.getItem("Database"))//access the main database
 const dashboardDOM = require("../dashboard_DOM");
+const Database = db.load();
 
 const usersCreator = register => {
     document.getElementById("regForm_saveButt").addEventListener("click", registration => {//when a user clicks save on registration form
         const regUsername = document.getElementById("regForm_username").value;//get value of username field
         const regEmail = document.getElementById("regForm_email").value;//get value of email field
         let nonExistant = true;//boolean for if user or email don't already exist
-        // Database.users.forEach(user => {//go through array of users
-        //     if (user.username === regUsername) {//check to see if username already exists
-        //         nonExistant = false;//if so set to false
-        //         alert("I'm sorry, that username already exists. Please enter a new user name.");//and inform user
-        //     } else if (user.email === regEmail) {//check to see if email already exists
-        //         nonExistant = false;//if so set to false
-        //         alert("I'm sorry, that email already exists. Please enter a new email.");//and inform user
-        //     };
-        // })
+        if (Database.users !== undefined) {
+            Database.users.forEach(user => {//go through array of users
+                if (user.username === regUsername) {//check to see if username already exists
+                    nonExistant = false;//if so set to false
+                    alert("I'm sorry, that username already exists. Please enter a new user name.");//and inform user
+                } else if (user.email === regEmail) {//check to see if email already exists
+                    nonExistant = false;//if so set to false
+                    alert("I'm sorry, that email already exists. Please enter a new email.");//and inform user
+                };
+            })
+        }
         if (nonExistant) {//if name and email don't exist
             usersFactory(regUsername, regEmail);//make new user object
             document.getElementById("regForm").classList.remove("d-flex")
