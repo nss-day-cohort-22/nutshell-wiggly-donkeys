d08d1f2990c6a198c556b19f8f91d1749c06387a
John Dulaney <john.s.dulaney1@gmail.com>
2017-11-13 10:37:08 -0600

    conflicts

--- scripts/articles/articleCreator.js
+++ scripts/articles/articleCreator.js
@@ -2,34 +2,66 @@
 // This file adds some starting structure for our Article Create Form.
 //┌(° ͜ʖ͡°)┘
 
-//imports
-const articleFactory = require("./articleFactory")
-// const Database = require("../Database")
-localStorage.setItem("Database", JSON.parse(Database))
+//the creator requires the messagesFactory
+const messagesFactory = require("./messagesFactory");
 
-//Create empty array if we need it
-Database.articles = Database.articles || []
+//pull the database from local storage
+const Database = JSON.parse(localStorage.getItem("Database"))
 
-// Add click event listener to the save button
-const saveArticleEl = document.getElementById("articleForm__saveButt")
-    addEventListener("click", event => {
+Database.messages = Database.messages || [];
 
-// Create a new article object
-    const newArticle = articleFactoryValue(
-        document.querySelector("input[name='articleForm__url']").value,
-        document.querySelector("input[name='articleForm__date']").value,
-        document.querySelector("textarea[name='articleForm__note']").value
-            )
+//find 'message' div in the html
+const messagesEl = document.getElementById("messages")
 
-    // Add new article to array
-    Database.articles.push(newArticle);
+//when the message save button is clicked, take what is in the message text box and store it in Database.messages, then push to local storage
+function messageStore () {
+    if (event.target.id === "messageForm_saveButt") {
+        const newMessage = messagesFactory(
+            document.getElementById("messageForm_text").value
+        );
+        Database.messages.push(newMessage);
+        localStorage.setItem("Database", JSON.stringify(Database));
+    }
+}
 
-    // Sort the articles by their `id` property, descending
-    Database.articles.sort((p, n) => n.id - p.id);
+//run the message button save event above that saves to the Database (messageStore) when anything is clicked in the message div element
+function messageListen () {
+    messagesEl.addEventListener("click", messageStore)
+}
 
-    localStorage.setItem("Database", JSON.stringify(Database))
-    document.forms["article__form"].reset();
+//run the messageListen function
+messageListen();
 
-})
+module.exports = Database
 
-module.exports = Database.articles
+// //imports
+// const articleFactory = require("./articleFactory")
+// // const Database = require("../Database")
+// localStorage.setItem("Database", JSON.parse(Database))
+
+// //Create empty array if we need it
+// Database.articles = Database.articles || []
+
+// // Add click event listener to the save button
+// const saveArticleEl = document.getElementById("articleForm__saveButt")
+//     addEventListener("click", event => {
+
+// // Create a new article object
+//     const newArticle = articleFactoryValue(
+//         document.querySelector("input[name='articleForm__url']").value,
+//         document.querySelector("input[name='articleForm__date']").value,
+//         document.querySelector("textarea[name='articleForm__note']").value
+//             )
+
+//     // Add new article to array
+//     Database.articles.push(newArticle);
+
+//     // Sort the articles by their `id` property, descending
+//     Database.articles.sort((p, n) => n.id - p.id);
+
+//     localStorage.setItem("Database", JSON.stringify(Database))
+//     document.forms["article__form"].reset();
+
+// })
+
+// module.exports = Database.articles
