68173b777b6475cff3b0593a6dfacee25c23ccaf
Paul Ellis <tynesellis@gmail.com>
2017-11-13 10:08:39 -0600

    connections DB added.  Function to make connections added

--- scripts/dashboard_DOM.js
+++ scripts/dashboard_DOM.js
@@ -1,7 +1,7 @@
 //Author: Paul Ellis; Purpose: function that populates the dashboard
 
 const Database = JSON.parse(localStorage.getItem("Database"))//access the main database
-const currentUser = JSON.parse(sessionStorage.getItem("currentUser"))//access the main database
+const currentUser = JSON.parse(sessionStorage.getItem("currentUser"))//access the user
 
 const populateDOM = DOM => {
     Database.innerDB.forEach(el => {
