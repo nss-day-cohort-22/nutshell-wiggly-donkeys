12d8381d15cd2f5eb38d655e6bb04482230f49fc
John Dulaney <john.s.dulaney1@gmail.com>
2017-11-14 12:17:37 -0600

    merge conflicts2

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,15 @@
 // Authors : Chase, John, Max, and Paul
 // Purpose : ¯\_(ツ)_/¯
-<<<<<<< HEAD
-const Database = require("./Database")
-=======
 
 const Database = require("./Database");
->>>>>>> master
 const usersRegister = require("./users/usersRegister");
 const usersLogin = require("./users/usersLogin");
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 // creators
-const articleCreator = require("./articles/articleCreator")
-const tasksCreator = require("./tasks/tasksCreator")
-=======
-=======
 const messagesPopulator = require("./messages/messagesPopulator")
->>>>>>> master
 const tasksCreator = require("./tasks/tasksCreator")
-
-// creators
 const messagesCreator = require("./messages/messagesCreator")
 const articleCreator = require("./articles/articleCreator");
 const storageListener = require("./storage-listener");
->>>>>>> master
 
 
