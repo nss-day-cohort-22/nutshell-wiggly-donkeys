539ad2b4da8ff85f273c475704ea8a22a8a67357
Chase Steely <chasesteely@gmail.com>
2017-11-14 10:48:43 -0600

    commiting so I can merge

--- scripts/tasks/tasksCreator.js
+++ scripts/tasks/tasksCreator.js
@@ -22,21 +22,25 @@ function taskStore() {
         document.getElementById("saveBtn").classList.remove("hidden")
     }
     if (event.target.id === "taskForm__saveButt") {
-        // Create a new task object
+        // Getting the values of new tasks from form
         const newTask = tasksFactory(
             document.querySelector("input[name='taskForm__taskName']").value,
             document.querySelector("input[name='taskForm__completionDate']").value
         );
+        //push the new message to the database
         Database.tasks.push(newTask);
         // Sort the task by their `id` property
         Database.tasks.sort((p, n) => p.taskId + n.taskId);
         db.save(Database);
+        //hide form and save button until new task button is clicked
         document.getElementById("taskForm").classList.add("hidden")
         document.getElementById("saveBtn").classList.add("hidden")
+        //clear form
         document.forms["taskForm"].reset();
+        //write new tasks to Dashboard
         taskDom()
     }
 }
 
-//run the message button save event above that saves to the Database (taskStore) when anything is clicked in the message div element
+//run the message button save event above that saves to the Database (taskStore) when anything is clicked in the task div element
 tasksEl.addEventListener("click", taskStore)
