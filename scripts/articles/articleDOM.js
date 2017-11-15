6b594e3f53ca34fa87b95d5e7a1eb6857bc66068
John Dulaney <john.s.dulaney1@gmail.com>
2017-11-15 14:20:29 -0600

    added comments and slight formatting to all my files

--- scripts/articles/articleDOM.js
+++ scripts/articles/articleDOM.js
@@ -4,16 +4,21 @@
 
 //imports
 const db = require("../Database")
-const deleteArticle  = require("./articleDelete")
+const deleteArticle = require("./articleDelete")
 
-//function
+//function that populates DOM using the form from ../innerHTML/articlesInner.js
 const articleDOM = () => {
+    //load db in this scope
     const Database = db.load()
+    //grab database.articles and set it, or, set it to empty array
     Database.articles = Database.articles || [];
+    //get html id set for population
     const articleEl = document.getElementById("article__post")
-        articleEl.innerHTML = ""
-             Database.articles.forEach(p => {
-                articleEl.innerHTML += `
+    //innerhtml clear the area
+    articleEl.innerHTML = ""
+    //foreach on articles, lists this innerHTML for each article in the DB and plugs correct info
+    Database.articles.forEach(p => {
+        articleEl.innerHTML += `
                 <article id=article__${p.id} class="article__card col-6">
                         <div>Article you shared on ${p.date}</div>
                         <div><a href="${p.url}">Link to Story</a></div>
@@ -21,11 +26,11 @@ const articleDOM = () => {
                         <input type="button" id="button__${p.id}" class="article__button" value="Delete this article">
                 </article>
                         `
-                    })
+    })
 
-
+    //add click event to the article that will delete it. Should probably be on a button to be honest...
     articleEl.addEventListener("click", deleteArticle)
-
 }
 
+//exports
 module.exports = articleDOM
--- scripts/events/eventsCreator.js
+++ scripts/events/eventsCreator.js
@@ -2,7 +2,7 @@
 // This file adds some starting structure for our event Create Form.
 //┌(° ͜ʖ͡°)┘
 
-//the creator requires the eventsFactory
+//imports
 const db = require("../Database")
 const eventsFactory = require("./eventsFactory");
 const eventsDOM = require("./eventsDOM")
@@ -12,6 +12,7 @@ const eventsEl = document.getElementById("events")
 
 //when the events save button is clicked, take what is in the events text box and store it in Database.events, then push to local storage
 function eventsStore() {
+    //load the database
     const Database = db.load()
     //Create an events key or make an empty array
     Database.events = Database.events || [];
@@ -19,6 +20,7 @@ function eventsStore() {
     // Sort the events by their `id` property, descending
     Database.events.sort((p, n) => n.id - p.id);
 
+    //if statement that makes a factory
     if (event.target.id === "eventForm__saveButt") {
         console.log("im working")
         const newEvents = eventsFactory(
@@ -27,11 +29,12 @@ function eventsStore() {
             document.querySelector("input[name='eventForm__location']").value
         );
 
+        //unshifts an article into the DB
         Database.events.unshift(newEvents);
 
-         //save to db
-         db.save(Database, "events")
-         
+        //save to db
+        db.save(Database, "events")
+
         //run dom function
         eventsDOM()
 
